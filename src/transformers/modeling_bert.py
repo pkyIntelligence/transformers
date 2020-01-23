1b5853508210d7eaf902ec797a5e193750a70a0f
Piseth Ky <ky.piseth@gmail.com>
2020-01-23 00:03:24 -0800

    adjusting bert model for captioning

--- src/transformers/modeling_bert.py
+++ src/transformers/modeling_bert.py
@@ -18,9 +18,11 @@
 
 import logging
 import math
+import numpy as np
 import os
 
 import torch
+import torch.nn.functional as F
 from torch import nn
 from torch.nn import CrossEntropyLoss, MSELoss
 
@@ -170,7 +172,8 @@ class BertEmbeddings(nn.Module):
         self.LayerNorm = BertLayerNorm(config.hidden_size, eps=config.layer_norm_eps)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
-    def forward(self, input_ids=None, token_type_ids=None, position_ids=None, inputs_embeds=None):
+    def forward(self, vis_feats, vis_pe, input_ids, token_type_ids=None, position_ids=None, inputs_embeds=None,
+                vis_input=True, len_vis_input=49):
         if input_ids is not None:
             input_shape = input_ids.size()
         else:
@@ -187,6 +190,13 @@ class BertEmbeddings(nn.Module):
         if inputs_embeds is None:
             inputs_embeds = self.word_embeddings(input_ids)
         position_embeddings = self.position_embeddings(position_ids)
+
+        if vis_input:
+            inputs_embeds = torch.cat((inputs_embeds[:, :1], vis_feats, inputs_embeds[:, len_vis_input+1:]), dim=1)
+            assert len_vis_input == 100, 'only support region attn!'
+            position_embeddings = torch.cat((position_embeddings[:, :1], vis_pe,
+                                             position_embeddings[:, len_vis_input+1]), dim=1)
+
         token_type_embeddings = self.token_type_embeddings(token_type_ids)
 
         embeddings = inputs_embeds + position_embeddings + token_type_embeddings
@@ -1461,3 +1471,338 @@ class BertForQuestionAnswering(BertPreTrainedModel):
             outputs = (total_loss,) + outputs
 
         return outputs  # (loss), start_logits, end_logits, (hidden_states), (attentions)
+
+
+class BertForImg2Txt(BertPreTrainedModel):
+    """refer to BertForPreTraining"""
+
+    def __init__(self, config, mask_word_id=0, num_labels=2,
+                 search_beam_size=1, length_penalty=1.0, eos_id=0,
+                 forbid_duplicate_ngrams=False, forbid_ignore_set=None,
+                 ngram_size=3, min_len=0, len_vis_input=49):
+        super().__init__(config)
+        self.bert = BertModel(config)
+        # TODO: do we need these? self.bert.embeddings.word_embeddings.weight, num_labels=num_labels
+        self.cls = BertPreTrainingHeads(config)
+        self.apply(self.init_bert_weights)
+        self.crit_mask_lm = nn.CrossEntropyLoss(reduction='none')
+        self.mask_word_id = mask_word_id
+        self.num_labels = num_labels
+        self.len_vis_input = len_vis_input
+        self.search_beam_size = search_beam_size
+        self.length_penalty = length_penalty
+        self.eos_id = eos_id
+        self.forbid_duplicate_ngrams = forbid_duplicate_ngrams
+        self.forbid_ignore_set = forbid_ignore_set
+        self.ngram_size = ngram_size
+        self.min_len = min_len
+
+        self.vis_embed = nn.Sequential(nn.Linear(1024, 1024),
+                                       nn.ReLU(),
+                                       nn.Linear(1024, config.hidden_size),
+                                       nn.ReLU(),
+                                       nn.Dropout(config.hidden_dropout_prob))
+        self.vis_pe_embed = nn.Sequential(nn.Linear(5+81, config.hidden_size),
+                                          nn.ReLU(),
+                                          nn.Dropout(config.hidden_dropout_prob))
+
+    def forward(self, vis_feats, vis_pe, input_ids, token_type_ids, position_ids, attention_mask, task_idx=None,
+                sample_mode='greedy'):
+
+        vis_feats = self.vis_embed(vis_feats)  # image region features
+        vis_pe = self.vis_pe_embed(vis_pe)  # image region positional encodings
+
+        if self.search_beam_size > 1:
+            return self.beam_search(vis_feats, vis_pe, input_ids, token_type_ids, position_ids, attention_mask, task_idx)
+        input_shape = list(input_ids.size())
+        batch_size = input_shape[0]
+        input_length = input_shape[1]
+        output_shape = list(token_type_ids.size())
+        output_length = output_shape[1]
+
+        output_ids = []
+        output_probs = []
+        prev_embedding = None
+        prev_encoded_layers = None
+        curr_ids = input_ids
+        mask_ids = input_ids[:, :1] * 0 + self.mask_word_id
+        next_pos = input_length
+
+        while next_pos < output_length:
+            curr_length = list(curr_ids.size())[1]
+            start_pos = next_pos - curr_length
+            x_input_ids = torch.cat((curr_ids, mask_ids), dim=1)
+            curr_token_type_ids = token_type_ids[:, start_pos:next_pos + 1]
+            curr_attention_mask = attention_mask[:, start_pos:next_pos + 1, :next_pos + 1]
+            curr_position_ids = position_ids[:, start_pos:next_pos + 1]
+            new_embedding, new_encoded_layers, _ = \
+                self.bert(vis_feats, vis_pe, x_input_ids, curr_token_type_ids, curr_position_ids,
+                          curr_attention_mask, prev_embedding=prev_embedding,
+                          prev_encoded_layers=prev_encoded_layers,
+                          output_all_encoded_layers=True, len_vis_input=self.len_vis_input)
+
+            last_hidden = new_encoded_layers[-1][:, -1:, :]
+            prediction_scores, _ = self.cls(last_hidden, None, task_idx=task_idx)
+            if sample_mode == 'greedy':
+                max_probs, max_ids = torch.max(prediction_scores, dim=-1)
+            elif sample_mode == 'sample':
+                prediction_scores.squeeze_(1)
+                prediction_probs = F.softmax(prediction_scores, dim=-1).detach()
+                max_ids = torch.multinomial(prediction_probs, num_samples=1, replacement=True)
+                max_probs = torch.gather(F.log_softmax(prediction_scores, dim=-1), 1, max_ids)  # this should be logprobs
+            else:
+                raise NotImplementedError
+            output_ids.append(max_ids)
+            output_probs.append(max_probs)
+            if prev_embedding is None:
+                prev_embedding = new_embedding[:, :-1, :]
+            else:
+                prev_embedding = torch.cat((prev_embedding, new_embedding[:, :-1, :]), dim=1)
+            if prev_encoded_layers is None:
+                prev_encoded_layers = [x[:, :-1, :] for x in new_encoded_layers]
+            else:
+                prev_encoded_layers = [torch.cat((x[0], x[1][:, :-1, :]), dim=1)
+                                       for x in zip(prev_encoded_layers, new_encoded_layers)]
+            curr_ids = max_ids
+            next_pos += 1
+        return torch.cat(output_ids, dim=1), torch.cat(output_probs, dim=1)
+
+    def beam_search(self, vis_feats, vis_pe, input_ids, token_type_ids, position_ids, attention_mask, task_idx=None):
+
+        input_shape = list(input_ids.size())
+        batch_size = input_shape[0]
+        input_length = input_shape[1]
+        output_shape = list(token_type_ids.size())
+        output_length = output_shape[1]
+
+        output_ids = []
+        prev_embedding = None
+        prev_encoded_layers = None
+        curr_ids = input_ids
+        mask_ids = input_ids[:, :1] * 0 + self.mask_word_id
+        next_pos = input_length
+
+        K = self.search_beam_size
+
+        total_scores = []
+        beam_masks = []
+        step_ids = []
+        step_back_ptrs = []
+        partial_seqs = []
+        forbid_word_mask = None
+        buf_matrix = None
+
+        while next_pos < output_length:
+            curr_length = list(curr_ids.size())[1]
+            start_pos = next_pos - curr_length
+            x_input_ids = torch.cat((curr_ids, mask_ids), dim=1)
+            curr_token_type_ids = token_type_ids[:, start_pos:next_pos + 1]
+            curr_attention_mask = attention_mask[:,
+                                                 start_pos:next_pos + 1, :next_pos + 1]
+            curr_position_ids = position_ids[:, start_pos:next_pos + 1]
+            new_embedding, new_encoded_layers, _ = \
+                self.bert(vis_feats, vis_pe, x_input_ids, curr_token_type_ids, curr_position_ids,
+                          curr_attention_mask, prev_embedding=prev_embedding,
+                          prev_encoded_layers=prev_encoded_layers,
+                          output_all_encoded_layers=True, len_vis_input=self.len_vis_input)
+
+            last_hidden = new_encoded_layers[-1][:, -1:, :]
+            prediction_scores, _ = self.cls(
+                last_hidden, None, task_idx=task_idx)
+            log_scores = torch.nn.functional.log_softmax(
+                prediction_scores, dim=-1)
+            if forbid_word_mask is not None:
+                log_scores += (forbid_word_mask * -10000.0)
+            if self.min_len and (next_pos-input_length+1 <= self.min_len):
+                log_scores[:, :, self.eos_id].fill_(-10000.0)
+            kk_scores, kk_ids = torch.topk(log_scores, k=K)
+            if len(total_scores) == 0:
+                k_ids = torch.reshape(kk_ids, [batch_size, K])
+                back_ptrs = torch.zeros(batch_size, K, dtype=torch.long)
+                k_scores = torch.reshape(kk_scores, [batch_size, K])
+            else:
+                last_eos = torch.reshape(
+                    beam_masks[-1], [batch_size * K, 1, 1])
+                last_seq_scores = torch.reshape(
+                    total_scores[-1], [batch_size * K, 1, 1])
+                kk_scores += last_eos * (-10000.0) + last_seq_scores
+                kk_scores = torch.reshape(kk_scores, [batch_size, K * K])
+                k_scores, k_ids = torch.topk(kk_scores, k=K)
+                back_ptrs = torch.div(k_ids, K)
+                kk_ids = torch.reshape(kk_ids, [batch_size, K * K])
+                k_ids = torch.gather(kk_ids, 1, k_ids)
+            step_back_ptrs.append(back_ptrs)
+            step_ids.append(k_ids)
+            beam_masks.append(torch.eq(k_ids, self.eos_id).float())
+            total_scores.append(k_scores)
+
+            def first_expand(x):
+                input_shape = list(x.size())
+                expanded_shape = input_shape[:1] + [1] + input_shape[1:]
+                x = torch.reshape(x, expanded_shape)
+                repeat_count = [1, K] + [1] * (len(input_shape) - 1)
+                x = x.repeat(*repeat_count)
+                x = torch.reshape(x, [input_shape[0] * K] + input_shape[1:])
+                return x
+
+            def select_beam_items(x, ids):
+                id_shape = list(ids.size())
+                id_rank = len(id_shape)
+                assert len(id_shape) == 2
+                x_shape = list(x.size())
+                x = torch.reshape(x, [batch_size, K] + x_shape[1:])
+                x_rank = len(x_shape) + 1
+                assert x_rank >= 2
+                if id_rank < x_rank:
+                    ids = torch.reshape(
+                        ids, id_shape + [1] * (x_rank - id_rank))
+                    ids = ids.expand(id_shape + x_shape[1:])
+                y = torch.gather(x, 1, ids)
+                y = torch.reshape(y, x_shape)
+                return y
+
+            is_first = (prev_embedding is None)
+
+            if prev_embedding is None:
+                prev_embedding = first_expand(new_embedding[:, :-1, :])
+            else:
+                prev_embedding = torch.cat(
+                    (prev_embedding, new_embedding[:, :-1, :]), dim=1)
+                prev_embedding = select_beam_items(prev_embedding, back_ptrs)
+            if prev_encoded_layers is None:
+                prev_encoded_layers = [first_expand(
+                    x[:, :-1, :]) for x in new_encoded_layers]
+            else:
+                prev_encoded_layers = [torch.cat((x[0], x[1][:, :-1, :]), dim=1)
+                                       for x in zip(prev_encoded_layers, new_encoded_layers)]
+                prev_encoded_layers = [select_beam_items(
+                    x, back_ptrs) for x in prev_encoded_layers]
+
+            curr_ids = torch.reshape(k_ids, [batch_size * K, 1])
+
+            if is_first:
+                token_type_ids = first_expand(token_type_ids)
+                position_ids = first_expand(position_ids)
+                attention_mask = first_expand(attention_mask)
+                mask_ids = first_expand(mask_ids)
+
+            if self.forbid_duplicate_ngrams:
+                wids = step_ids[-1].tolist()
+                ptrs = step_back_ptrs[-1].tolist()
+                if is_first:
+                    partial_seqs = []
+                    for b in range(batch_size):
+                        for k in range(K):
+                            partial_seqs.append([wids[b][k]])
+                else:
+                    new_partial_seqs = []
+                    for b in range(batch_size):
+                        for k in range(K):
+                            new_partial_seqs.append(
+                                partial_seqs[ptrs[b][k] + b * K] + [wids[b][k]])
+                    partial_seqs = new_partial_seqs
+
+                def get_dup_ngram_candidates(seq, n):
+                    cands = set()
+                    if len(seq) < n:
+                        return []
+                    tail = seq[-(n-1):]
+                    if self.forbid_ignore_set and any(tk in self.forbid_ignore_set for tk in tail):
+                        return []
+                    for i in range(len(seq) - (n - 1)):
+                        mismatch = False
+                        for j in range(n - 1):
+                            if tail[j] != seq[i + j]:
+                                mismatch = True
+                                break
+                        if (not mismatch) and not(self.forbid_ignore_set and (seq[i + n - 1] in self.forbid_ignore_set)):
+                            cands.add(seq[i + n - 1])
+                    return list(sorted(cands))
+
+                if len(partial_seqs[0]) >= self.ngram_size:
+                    dup_cands = []
+                    for seq in partial_seqs:
+                        dup_cands.append(
+                            get_dup_ngram_candidates(seq, self.ngram_size))
+                    if max(len(x) for x in dup_cands) > 0:
+                        if buf_matrix is None:
+                            vocab_size = list(log_scores.size())[-1]
+                            buf_matrix = np.zeros(
+                                (batch_size * K, vocab_size), dtype=float)
+                        else:
+                            buf_matrix.fill(0)
+                        for bk, cands in enumerate(dup_cands):
+                            for i, wid in enumerate(cands):
+                                buf_matrix[bk, wid] = 1.0
+                        forbid_word_mask = torch.tensor(
+                            buf_matrix, dtype=log_scores.dtype)
+                        forbid_word_mask = torch.reshape(
+                            forbid_word_mask, [batch_size * K, 1, vocab_size]).cuda()
+                    else:
+                        forbid_word_mask = None
+            next_pos += 1
+
+        # [(batch, beam)]
+        total_scores = [x.tolist() for x in total_scores]
+        step_ids = [x.tolist() for x in step_ids]
+        step_back_ptrs = [x.tolist() for x in step_back_ptrs]
+        # back tracking
+        traces = {'pred_seq': [], 'scores': [], 'wids': [], 'ptrs': []}
+        for b in range(batch_size):
+            # [(beam,)]
+            scores = [x[b] for x in total_scores]
+            wids_list = [x[b] for x in step_ids]
+            ptrs = [x[b] for x in step_back_ptrs]
+            traces['scores'].append(scores)
+            traces['wids'].append(wids_list)
+            traces['ptrs'].append(ptrs)
+            # first we need to find the eos frame where all symbols are eos
+            # any frames after the eos frame are invalid
+            last_frame_id = len(scores) - 1
+            for i, wids in enumerate(wids_list):
+                if all(wid == self.eos_id for wid in wids):
+                    last_frame_id = i
+                    break
+            max_score = -math.inf
+            frame_id = -1
+            pos_in_frame = -1
+
+            for fid in range(last_frame_id + 1):
+                for i, wid in enumerate(wids_list[fid]):
+                    if wid == self.eos_id or fid == last_frame_id:
+                        s = scores[fid][i] + self.length_penalty * (fid + 1)
+                        if s > max_score:
+                            max_score = s
+                            frame_id = fid
+                            pos_in_frame = i
+            if frame_id == -1:
+                traces['pred_seq'].append([0])
+            else:
+                seq = [wids_list[frame_id][pos_in_frame]]
+                for fid in range(frame_id, 0, -1):
+                    pos_in_frame = ptrs[fid][pos_in_frame]
+                    seq.append(wids_list[fid - 1][pos_in_frame])
+                seq.reverse()
+                traces['pred_seq'].append(seq)
+
+        def _pad_sequence(sequences, max_len, padding_value=0):
+            trailing_dims = sequences[0].size()[1:]
+            out_dims = (len(sequences), max_len) + trailing_dims
+
+            out_tensor = sequences[0].data.new(*out_dims).fill_(padding_value)
+            for i, tensor in enumerate(sequences):
+                length = tensor.size(0)
+                # use index notation to prevent duplicate references to the tensor
+                out_tensor[i, :length, ...] = tensor
+            return out_tensor
+
+        # convert to tensors for DataParallel
+        for k in ('pred_seq', 'scores', 'wids', 'ptrs'):
+            ts_list = traces[k]
+            if not isinstance(ts_list[0], torch.Tensor):
+                dt = torch.float if k == 'scores' else torch.long
+                ts_list = [torch.tensor(it, dtype=dt) for it in ts_list]
+            traces[k] = _pad_sequence(
+                ts_list, output_length, 0).to(input_ids.device)
+
+        return traces
